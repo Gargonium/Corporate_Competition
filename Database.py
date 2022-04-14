9a19478854cad53fd920fb6a79e0f4a32526efb0
Gargonium <tetatet.nikvot@gmail.com>
2022-04-14 18:50:10 +0700

    Dataset

--- Database.py
+++ Database.py
@@ -3,14 +3,19 @@ from random import random
 
 f = open('dataset.txt', 'w')
 
-for _ in range(1000):
+for _ in range(300):
     Rand = random()
     for __ in range(1000):
         AI = random()
+        AIp, Randp = Start(AI, Rand)
+        AI = int(AI * 1000)
+        Rand = int(Rand*1000)
+        Randp = int(Randp * 1000)
+        AIp = int(AIp * 1000)
         f.write(str(AI) + ' ')
         f.write(str(Rand) + ' ')
-        AIp, Randp = Start(AI, Rand)
         f.write(str(AIp) + ' ')
         f.write(str(Randp) + ' ')
         f.write('\n')
+        Rand = Rand/1000
 
